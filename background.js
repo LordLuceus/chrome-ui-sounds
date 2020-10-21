b1c8352591046216c93db64f3eccfe005773343c
LordLuceus <lord.luceus@gmail.com>
2020-10-21 16:24:21 +0200

    A much better way, allowing for future custom user sounds and customisable volume

--- background.js
+++ background.js
@@ -1,15 +1,43 @@
-const tabUpdate = new Audio("tab_update.wav");
-const tabSwitch = new Audio("switch.wav");
-const newTab = new Audio("new_tab.wav");
+// These variables will store our Audio objects.
+let tabUpdate;
+let tabSwitch;
+let tabNew;
+let tabClose;
 
-chrome.tabs.onUpdated.addListener(() => {
-    tabUpdate.play();
+// Check if the user selected a custom sound, use default if not.
+const getSound = (sound) => {
+    if (localStorage.getItem(sound)) {
+        return new Audio(localStorage.getItem(sound));
+    }
+    return new Audio(`${sound}.wav`);
+};
+
+// Play the sound
+const playSound = (sound) => {
+    if (localStorage.getItem("volume")) {
+        sound.volume = localStorage.getItem("volume");
+    }
+    sound.play();
+};
+
+const { storage, tabs } = chrome;
+
+tabs.onActivated.addListener(() => {
+    tabSwitch = getSound("tabSwitch");
+    playSound(tabSwitch);
 });
 
-chrome.tabs.onActivated.addListener(() => {
-    tabSwitch.play();
+tabs.onUpdated.addListener(() => {
+    tabUpdate = getSound("tabUpdate");
+    playSound(tabUpdate);
 });
 
-chrome.tabs.onCreated.addListener(() => {
-    newTab.play();
+tabs.onCreated.addListener(() => {
+    tabNew = getSound("tabNew");
+    playSound(tabNew);
+});
+
+tabs.onRemoved.addListener(() => {
+    tabClose = getSound("tabClose");
+    playSound(tabClose);
 });
