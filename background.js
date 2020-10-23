6783c37a9c804b556ccb06f4bc21b72dac004e04
LordLuceus <lord.luceus@gmail.com>
2020-10-23 09:02:56 +0200

    Refactor the options functions a bit

--- background.js
+++ background.js
@@ -13,11 +13,13 @@ const setSound = (sound) => {
     return sounds[sound];
 };
 
-// Play the sound
+// Check the volume and if the sound is enabled, then play it.
 const playSound = (sound, name) => {
-    if (localStorage.getItem("volume")) {
-        sound.volume = localStorage.getItem("volume");
-    }
+    storage.sync.get("globalVolume", (result) => {
+        if (result.globalVolume) {
+            sound.volume = result.globalVolume;
+        }
+    });
     storage.sync.get(`${name}Enabled`, (result) => {
         if (result[name + "Enabled"]) {
             sound.play();
@@ -29,7 +31,7 @@ const { storage, tabs } = chrome;
 
 tabs.onActivated.addListener(() => {
     sounds.tabSwitch = setSound("tabSwitch");
-    playSound(sounds.tabSwitch);
+    playSound(sounds.tabSwitch, "tabSwitch");
 });
 
 tabs.onUpdated.addListener(() => {
@@ -39,10 +41,10 @@ tabs.onUpdated.addListener(() => {
 
 tabs.onCreated.addListener(() => {
     sounds.tabNew = setSound("tabNew");
-    playSound(sounds.tabNew);
+    playSound(sounds.tabNew, "tabNew");
 });
 
 tabs.onRemoved.addListener(() => {
     sounds.tabClose = setSound("tabClose");
-    playSound(sounds.tabClose);
+    playSound(sounds.tabClose, "tabClose");
 });
--- options.js
+++ options.js
@@ -3,10 +3,18 @@ const { storage } = chrome;
 // This function takes an array of elements, iterates over them, and saves each option to Chrome storage appropriately
 const saveOptions = (options) => {
     options.forEach((element, index) => {
+        // Make sure the UI follows the state.
         if (index === 0) {
             storage.sync.get(`${element.parentElement.id}Enabled`, (result) => {
                 if (!result[element.parentElement.id + "Enabled"]) {
-                    element.checked = false;
+                    if (
+                        result[element.parentElement.id + "Enabled"] ===
+                        undefined
+                    ) {
+                        // We haven't put anything in storage, so do nothing.
+                    } else {
+                        element.checked = false;
+                    }
                 }
             });
         }
@@ -20,17 +28,18 @@ const saveOptions = (options) => {
     });
 };
 
-const getOptions = (optionGroup) => {
-    const options = [
+const populateOptionsArray = (optionGroup) => {
+    return [
         optionGroup.querySelector(".toggle"),
         optionGroup.querySelector(".volume"),
         optionGroup.querySelector(".custom")
     ];
+};
+
+const executeOptions = (optionGroup) => {
+    const options = populateOptionsArray(optionGroup);
     saveOptions(options);
 };
 
-getOptions(document.querySelector("#tabUpdate"));
-
-// storage.sync.get(null, (result) => {
-//     console.log(result);
-// });
+executeOptions(document.querySelector("#tabUpdate"));
+executeOptions(document.querySelector("#tabSwitch"));
